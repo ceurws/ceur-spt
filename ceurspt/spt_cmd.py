bc4c4d8447a31fa4bf19f32efc364f25bfcd6c97
tholzheim <tim.holzheim@rwth-aachen.de>
2023-03-17 16:57:46 +0100

    fixed formatting and updated requirements

--- ceurspt/spt_cmd.py
+++ ceurspt/spt_cmd.py
@@ -1,9 +1,9 @@
-'''
+"""
 Created on 2023-03-17
 
 @author: wf
-'''
-from argparse import ArgumentParser
+"""
+from argparse import ArgumentParser, Namespace
 from argparse import RawDescriptionHelpFormatter
 from ceurspt.version import Version
 from ceurspt.webserver import WebServer
@@ -13,12 +13,13 @@ import traceback
 import webbrowser
 import uvicorn
 
+
 class CeurSptCmd:
     """
     command line interface for CEUR Single Point of Truth
     """
-    
-    def get_arg_parser(self,description:str,version_msg)->ArgumentParser:
+
+    def get_arg_parser(self, description: str, version_msg) -> ArgumentParser:
         """
         Setup command line argument parser
         
@@ -30,14 +31,16 @@ class CeurSptCmd:
             ArgumentParser: the argument parser
         """
         parser = ArgumentParser(description=description, formatter_class=RawDescriptionHelpFormatter)
-        parser.add_argument("-a","--about",help="show about info [default: %(default)s]",action="store_true")
-        parser.add_argument("-d", "--debug", dest="debug", action="store_true", help="show debug info [default: %(default)s]")
-        parser.add_argument("--host",default=self.get_default_host(),help="the host to serve / listen from [default: %(default)s]")
-        parser.add_argument("--port",type=int,default=9990,help="the port to serve from [default: %(default)s]")
-        parser.add_argument("-s","--serve", action="store_true", help="start webserver [default: %(default)s]")
+        parser.add_argument("-a", "--about", help="show about info [default: %(default)s]", action="store_true")
+        parser.add_argument("-d", "--debug", dest="debug", action="store_true",
+                            help="show debug info [default: %(default)s]")
+        parser.add_argument("--host", default=self.get_default_host(),
+                            help="the host to serve / listen from [default: %(default)s]")
+        parser.add_argument("--port", type=int, default=9990, help="the port to serve from [default: %(default)s]")
+        parser.add_argument("-s", "--serve", action="store_true", help="start webserver [default: %(default)s]")
         parser.add_argument("-V", "--version", action='version', version=version_msg)
-        return parser   
-    
+        return parser
+
     def get_default_host(self) -> str:
         """
         get the default host as the fully qualifying hostname
@@ -46,35 +49,36 @@ class CeurSptCmd:
         Returns:
             str: the hostname
         """
-        host = socket.getfqdn() 
+        host = socket.getfqdn()
         # work around https://github.com/python/cpython/issues/79345
-        if host=="1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa":
-            host="localhost"
-            # host="127.0.0.1"
+        if host == "1.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.0.ip6.arpa":
+            host = "localhost"  # host="127.0.0.1"
         return host
-    
-    def start(self,args):
+
+    def start(self, args: Namespace):
         """
         Args:
             args(Arguments): command line arguments
         """
-        ws=WebServer()        
-        uvicorn.run(ws.app,host=args.host, port=args.port, debug=args.debug)
-    
-def main(argv=None): # IGNORE:C0111
-    '''main program.'''
+        ws = WebServer()
+        uvicorn.run(ws.app, host=args.host, port=args.port)
+
+
+def main(argv=None):  # IGNORE:C0111
+    """main program."""
 
     if argv is None:
-        argv=sys.argv[1:]
-        
+        argv = sys.argv[1:]
+
     program_name = "ceurspt"
-    program_version =f"v{Version.version}" 
+    program_version = f"v{Version.version}"
     program_build_date = str(Version.date)
     program_version_message = f'{program_name} ({program_version},{program_build_date})'
 
+    args = None
     try:
-        spt_cmd=CeurSptCmd()
-        parser=spt_cmd.get_arg_parser(description=Version.license,version_msg=program_version_message)
+        spt_cmd = CeurSptCmd()
+        parser = spt_cmd.get_arg_parser(description=Version.license, version_msg=program_version_message)
         args = parser.parse_args(argv)
         if len(argv) < 1:
             parser.print_usage()
@@ -85,20 +89,25 @@ def main(argv=None): # IGNORE:C0111
             webbrowser.open(Version.doc_url)
         elif args.serve:
             spt_cmd.start(args)
-    
+
     except KeyboardInterrupt:
-        ### handle keyboard interrupt ###
+        ###
+        # handle keyboard interrupt
+        # ###
         return 1
     except Exception as e:
         if DEBUG:
-            raise(e)
+            raise e
         indent = len(program_name) * " "
         sys.stderr.write(program_name + ": " + repr(e) + "\n")
         sys.stderr.write(indent + "  for help use --help")
-        if args.debug:
+        if args is None:
+            print("args could not be parsed")
+        elif args.debug:
             print(traceback.format_exc())
-        return 2       
-    
+        return 2
+
+
 DEBUG = 1
 if __name__ == "__main__":
     if DEBUG:
